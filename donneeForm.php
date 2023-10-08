a6aabce4f16804728462bc0a8481ec5e487ec1cb
DrPestilent <108067408+DrPestilent@users.noreply.github.com>
2023-10-08 03:35:31 +0200

    update

--- donneeForm.php
+++ donneeForm.php
@@ -1,57 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    $servername = "192.168.1.11"; // L'adresse IP du serveur MySQL
-    $username = "root"; // Le nom d'utilisateur MySQL
-    $password = "HunterX2011!"; // Le mot de passe MySQL
-    $dbname = "contacts"; // Le nom de la base de données
-    
-    // Établir une connexion
+    // Récupérer les données du formulaire
+    $nom = $_REQUEST['nom'];
+    $email = $_REQUEST['email'];
+    $phone = $_REQUEST['phone'];
+    $message = $_REQUEST['message'];
+    echo "voici les informations que vous avez entré : $nom  $email $phone $message";
+    // Connexion à la base de données
+    $servername = "192.168.1.11";
+    $username = "root";
+    $password = "HunterX2011!";
+    $dbname = "contacts";
+
     $conn = new mysqli($servername, $username, $password, $dbname);
+
     
+
+
     // Vérifier la connexion
     if ($conn->connect_error) {
-      die("La connexion a échoué : " . $conn->connect_error);
+        die("La connexion a échoué : " . $conn->connect_error);
     }
-    
-    echo "Connexion réussie";
-    
-    // Vous pouvez maintenant exécuter des requêtes SQL ici
-    $sql = "SELECT * FROM nom_de_la_table";
-    $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-    // Traiter les données
+    // Préparer et exécuter la requête SQL
+    $sql = "INSERT INTO nom_de_votre_table (nom, email, phone, message) VALUES ('$nom', '$email', '$phone', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Enregistrement créé avec succès.";
     } else {
-  echo "Aucun résultat trouvé";
-}
-    // Fermer la connexion
-    $conn->close();
-
-
-
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nom = $_REQUEST["nom"];
-        $email = $_REQUEST["email"];
-        $message = $_REQUEST["message"];
-
-        $to = "rodrigue.baste@proton.me";
-        $subject = "Nouveau message du formulaire de contact";
-        $body = "Nom: $nom\nE-mail: $email\n\nMessage:\n$message";
-
-        if (mail($to, $subject, $body)) {
-            echo "Votre message a été envoyé avec succès.";
-        } else {
-            echo "Une erreur s'est produite lors de l'envoi du message.";
-        }
+        echo "Erreur : " . $sql . "<br>" . $conn->error;
     }
+
+    // Fermer la connexion à la base de données
+    $conn->close();
     ?>
 </body>
+
 </html>
