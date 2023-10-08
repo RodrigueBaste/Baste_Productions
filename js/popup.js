175256b2f9ca5a656d1bb89053c0ef2cb09824f6
DrPestilent <108067408+DrPestilent@users.noreply.github.com>
2023-10-08 19:12:43 +0200

    ajout de popup

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+function openPopup(elementId) {
+    const popup = document.getElementById(elementId);
+    popup.style.display = 'flex';
+}
+
+function closePopup(elementId) {
+    const popup = document.getElementById(elementId);
+    popup.style.display = 'none';
+}
